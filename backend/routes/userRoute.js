c54e3802a1ba73c68225fe4fb5fa5fe818a2aa14
Aamirkhan121 <131248243+Aamirkhan121@users.noreply.github.com>
2025-12-12 15:47:58 +0530

    Update userRoute.js

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -14,7 +14,7 @@ router.delete("/delete/:username", auth, deleteUser);
 // Protected route to get user profile
 router.get('/profile/:username',auth, getUserProfileByUsername);
 // routes/user.js
-router.put('/profile/:username', auth, updateUserProfile);
+router.put('/profile/:username', auth,upload.single("avatar"), updateUserProfile);
 
 
 module.exports = router;
